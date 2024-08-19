0331ffd73fd10d27fe7617cc4a9b9d80905db309
Cristiely  Martins <104570782+ismeCris@users.noreply.github.com>
2024-08-18 22:16:34 -0300

    Delete cat01 directory

--- cat01/src/main/java/app/Repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package app.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.Entity.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-}
--- cat01/src/main/java/app/Repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package app.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.Entity.Produto;
-import app.Entity.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-}
