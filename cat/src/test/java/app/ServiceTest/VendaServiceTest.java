980b5fb67298b2450c423651c996826e90fcf553
ismycris <cristielythainara@gmail.com>
2024-09-10 21:02:09 -0300

    update prom

--- cat/src/test/java/app/ServiceTest/VendaServiceTest.java
+++ cat/src/test/java/app/ServiceTest/VendaServiceTest.java
@@ -1,5 +1,30 @@
 package app.ServiceTest;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import app.Service.ProdutoService;
+import app.Service.UsuarioService;
+import app.Service.VendaService;
+
+@SpringBootTest
 public class VendaServiceTest {
+	
+	@Autowired
+	VendaService vendaService;
+	
+	@MockBean
+	ProdutoService produtoService;
+	
+	@MockBean
+	UsuarioService usuarioService;
+	
+	@BeforeEach
+	void setup() {
+		
+	}
+	
 
 }
