34ff72e9aa5d52e399e743cbbbe44318b2450120
willianvozes <contatowillpm@gmail.com>
2024-09-22 23:31:46 -0300

    TESTES produto service e mini ajuste em service de produto

--- cat/src/main/java/app/Service/ProdutoService.java
+++ cat/src/main/java/app/Service/ProdutoService.java
@@ -15,7 +15,6 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 
-
 	public String save(Produto produto) {
 
 		if (conferirProd(produto)) {
@@ -23,36 +22,35 @@ public class ProdutoService {
 		}
 
 		produto.setAtivo(true);
-        this.produtoRepository.save(produto);
-        return "Produto salvo com sucesso";
+		this.produtoRepository.save(produto);
+		return "Produto salvo com sucesso";
 	}
 
-
 	public String update(Produto produto, long id) {
-		 produto.setId(id);
+		produto.setId(id);
 
-	        if (conferirProd(produto)) {
-	            throw new RuntimeException("Produto com o mesmo nome e descrição já existe");
-	        }
-	
+		if (conferirProd(produto)) {
+			throw new RuntimeException("Produto com o mesmo nome e descrição já existe");
+		}
+
 		this.produtoRepository.save(produto);
-		return "Atualizado com sucesso";
+		return "Produto atualizado com sucesso";
 	}
 
 	public boolean conferirProd(Produto produto) {
-	    Optional<Produto> existingProduct = produtoRepository.findByNomeAndDescricao(produto.getNome(), produto.getDescricao());
-	    
-	    // Verifica se o produto existe e se o ID é diferente do ID do produto que está sendo atualizado
-	    return existingProduct.isPresent() && existingProduct.get().getId() != produto.getId();
-	}
+		Optional<Produto> existingProduct = produtoRepository.findByNomeAndDescricao(produto.getNome(),
+				produto.getDescricao());
 
+		// Verifica se o produto existe e se o ID é diferente do ID do produto que está
+		// sendo atualizado
+		return existingProduct.isPresent() && existingProduct.get().getId() != produto.getId();
+	}
 
 	public Produto findById(long id) {
 		Optional<Produto> optional = this.produtoRepository.findById(id);
 		if (optional.isPresent()) {
 			return optional.get();
-		} else
-			return null;
+		} else return null;
 	}
 
 	public List<Produto> findAll(boolean ativo) {
@@ -63,22 +61,23 @@ public class ProdutoService {
 		this.produtoRepository.deleteById(id);
 		return "Produto deletado com sucesso!";
 	}
-	
+
 	public String disable(Long id) {
-		Produto produtoInDB = this.findById(id);
-		produtoInDB.setAtivo(false);
-		this.produtoRepository.save(produtoInDB);
-		return "Produto desativado com sucesso!";
+	    Produto produtoInDB = this.findById(id);
+	    if (produtoInDB == null) {
+	        throw new RuntimeException("Produto não encontrado");
+	    }
+	    produtoInDB.setAtivo(false);
+	    this.produtoRepository.save(produtoInDB);
+	    return "Produto desativado com sucesso!";
 	}
-	
+
+
 	public String enable(Long id) {
 		Produto produtoInDB = this.findById(id);
 		produtoInDB.setAtivo(true);
 		this.produtoRepository.save(produtoInDB);
 		return "Produto ativado com sucesso!";
 	}
-	
+
 }
-
-
-
