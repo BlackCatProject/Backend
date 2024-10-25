ebf992d06e9b88b12bfbc3f7064062bbedbc58cc
willkiwi <contatowillpm@gmail.com>
2024-10-25 17:24:06 -0300

    crossOrigin apenas

--- cat/src/main/java/app/Controller/UsuarioController.java
+++ cat/src/main/java/app/Controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import app.Service.UsuarioService;
 @Validated
 @RestController
 @RequestMapping("api/usuario")
+@CrossOrigin("*")
 public class UsuarioController {
 
 	@Autowired
