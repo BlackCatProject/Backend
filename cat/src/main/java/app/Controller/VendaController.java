307130311aa23dd3d18d10562768e119ef54d2b0
Maczevski <maczevski@gmail.com>
2024-10-25 22:25:04 -0300

    cross origin

--- cat/src/main/java/app/Controller/VendaController.java
+++ cat/src/main/java/app/Controller/VendaController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import jakarta.validation.Valid;
 @Validated
 @RestController
 @RequestMapping("/api/venda")
+@CrossOrigin("*")
 public class VendaController {
 	@Autowired
 	private VendaService vendaService;
