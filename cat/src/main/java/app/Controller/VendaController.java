9d3941e2a78099a77d95de0ae0b65d8a72291935
Maczevski <maczevski@gmail.com>
2024-11-23 20:40:34 -0300

    Verificando usuario logado antes de desativar

--- cat/src/main/java/app/Controller/VendaController.java
+++ cat/src/main/java/app/Controller/VendaController.java
@@ -20,9 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import app.Entity.Venda;
-import app.Service.UsuarioService;
 import app.Service.VendaService;
-import app.auth.Usuario;
 import jakarta.validation.Valid;
 
 @Validated
@@ -33,17 +31,14 @@ public class VendaController {
 	@Autowired
 	private VendaService vendaService;
 
-	@Autowired
-	private UsuarioService usuarioService;  // Injeção do serviço de usuário
-
 	@PostMapping("/save")
 	public ResponseEntity<String> save(@RequestBody @Valid Venda venda) {
-//		try {
+		try {
 			String msn = this.vendaService.save(venda);
 			return new ResponseEntity<>(msn, HttpStatus.OK);
-//		} catch (Exception e) {
-//			return new ResponseEntity<>("Deu Erro! " + e.getMessage(), HttpStatus.BAD_REQUEST);
-//		}
+		} catch (Exception e) {
+			return new ResponseEntity<>("Deu Erro! " + e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	}
 
 	@PutMapping("/update/{id}")
@@ -156,24 +151,6 @@ public class VendaController {
             return new ResponseEntity<>(0.0, HttpStatus.BAD_REQUEST);
         }
     }
-    @GetMapping("/historico-vendas")
-    public ResponseEntity<List<Venda>> getHistoricoVendasFuncionario() {
-        try {
-            Usuario usuarioLogado = usuarioService.getUsuarioLogado();
-
-            List<Venda> vendasFuncionario = vendaService.findByUsuarioId(usuarioLogado.getId());
-            
-            if (vendasFuncionario.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            return new ResponseEntity<>(vendasFuncionario, HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
 
    
 }
--- cat/src/main/java/app/Service/UsuarioService.java
+++ cat/src/main/java/app/Service/UsuarioService.java
@@ -1,4 +1,5 @@
 package app.Service;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -6,8 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.Authentication;  // Corrigido para a importação correta do Spring
+import org.springframework.security.core.Authentication; // Corrigido para a importação correta do Spring
 
+import app.auth.LoginService;
 import app.auth.Usuario;
 import app.Repository.UsuarioRepository;
 
@@ -16,15 +18,15 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
- @Autowired
- private BCryptPasswordEncoder bCryptPasswordEncoder;
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 
 	public String save(Usuario usuario) {
 
 		if (conferirUser(usuario)) {
 			throw new RuntimeException("Login ou Senha já está em uso");
 		}
-
 
 		usuario.setAtivo(true);
 		usuario.setSenha(this.bCryptPasswordEncoder.encode(usuario.getSenha()));
@@ -33,88 +35,97 @@ public class UsuarioService {
 	}
 
 	public String update(Usuario usuario, long id) {
-		 // Define o ID no objeto usuário antes de verificar a existência de login duplicado
-	    usuario.setId(id);
-	    
-	    if (conferirUser(usuario)) {
-	        throw new RuntimeException("Login ou Senha já está em uso");
-	    }
-	    Optional<Usuario> optional = this.usuarioRepository.findById(id);
+		// Define o ID no objeto usuário antes de verificar a existência de login
+		// duplicado
+		usuario.setId(id);
+
+		if (conferirUser(usuario)) {
+			throw new RuntimeException("Login ou Senha já está em uso");
+		}
+		Optional<Usuario> optional = this.usuarioRepository.findById(id);
 		if (optional.isPresent()) {
 			Usuario userInDB = findById(id);
-			if(!bCryptPasswordEncoder.matches(usuario.getSenha(),userInDB.getSenha())) {
-		    	usuario.setSenha(this.bCryptPasswordEncoder.encode(usuario.getSenha()));
-		    }
+			if (!bCryptPasswordEncoder.matches(usuario.getSenha(), userInDB.getSenha())) {
+				usuario.setSenha(this.bCryptPasswordEncoder.encode(usuario.getSenha()));
+			}
 			this.usuarioRepository.save(usuario);
 			System.out.println("Chegou aqui");
 			return "Atualizado com sucesso";
-		}else {
+		} else {
 			throw new RuntimeException("Usúario não encontrado");
 		}
-	    
+
 	}
 
 	public boolean conferirUser(Usuario usuario) {
 		Optional<Usuario> existingUser = usuarioRepository.findByLogin(usuario.getLogin());
-		// Verifica se o usuário existe e se o ID é diferente do ID do usuário que está sendo atualizado
-		
+		// Verifica se o usuário existe e se o ID é diferente do ID do usuário que está
+		// sendo atualizado
+
 		return existingUser.isPresent() && existingUser.get().getId() != usuario.getId();
 	}
-	
 
 	public Usuario findById(long id) {
 		Optional<Usuario> optional = this.usuarioRepository.findById(id);
 		if (optional.isPresent()) {
 			optional.get().setSenha("");
 			return optional.get();
-		}else
+		} else
 			throw new RuntimeException("Usúario não encontrado");
 	}
 
 	public List<Usuario> findAll(boolean ativo) {
-		
+
 		List<Usuario> users = this.usuarioRepository.findByAtivo(ativo);
-		
-		for(Usuario user : users) {
+
+		for (Usuario user : users) {
 			user.setSenha("");
 		}
-		
+
 		return this.usuarioRepository.findByAtivo(ativo);
 	}
-	
+
 	public String delete(Long id) {
 		this.usuarioRepository.deleteById(id);
 		return "Usuario deletado com sucesso";
 	}
-	
+
 	public String disable(Long id) {
+		
+		Usuario userLogado = this.getUsuarioLogado();
+		System.out.println(userLogado.getNome());
+		
 		Optional<Usuario> optional = this.usuarioRepository.findById(id);
-		if(optional.isPresent()) {
+		if (optional.isPresent()) {
 			Usuario usuarioInDB = optional.get();
-			usuarioInDB.setAtivo(false);
-			this.usuarioRepository.save(usuarioInDB);
-			return "Usuário desativado com sucesso!";
-		}else{
+			if (usuarioInDB.getId() != userLogado.getId()) {
+				usuarioInDB.setAtivo(false);
+				this.usuarioRepository.save(usuarioInDB);
+				return "Usuário desativado com sucesso!";
+			} else {
+				throw new RuntimeException("Não é possível se autodesativar");
+			}
+		} else {
 			throw new RuntimeException("Usúario não encontrado");
 		}
 	}
-	
+
 	public String enable(Long id) {
 		Optional<Usuario> optional = this.usuarioRepository.findById(id);
-		if(optional.isPresent()) {
+		if (optional.isPresent()) {
 			Usuario usuarioInDB = optional.get();
 			usuarioInDB.setAtivo(true);
 			this.usuarioRepository.save(usuarioInDB);
 			return "Usuário ativado com sucesso!";
-		}else{
+		} else {
 			throw new RuntimeException("Usúario não encontrado");
-		}	
+		}
 	}
 
-	 public Usuario getUsuarioLogado() {
-	        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-	        String username = authentication.getName(); // Aqui assumimos que o login é o username
-	        return usuarioRepository.findByLogin(username)
-	                .orElseThrow(() -> new RuntimeException("Usuário logado não encontrado"));
-	    }
+	public Usuario getUsuarioLogado() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String username = authentication.getName(); // Aqui assumimos que o login é o username
+		return usuarioRepository.findByLogin(username)
+				.orElseThrow(() -> new RuntimeException("Usuário logado não encontrado"));
+	}
 }
