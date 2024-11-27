9eaaa6b66fc5bc73dc92a12d12daf18f4f2271f1
ismycris <cristielythainara@gmail.com>
2024-11-26 21:35:06 -0300

    batata

--- cat/src/main/java/app/auth/LoginController.java
+++ cat/src/main/java/app/auth/LoginController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/login")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin("*")
 public class LoginController {
 
 	@Autowired
